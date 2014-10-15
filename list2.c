c3db16598398706ee3d056b149f14df5a5bbbf16
aucguy <jhucguy@gmail.com>
2014-10-15 16:55:06 -0500

    implement linked list
    asdf

---
+++ list2.c
@@ -0,0 +1,18 @@
+/*
+functions:
+ListNew(); -> list
+ListDelete(List list); -> void
+ListAddFirst(List list, T item); -> void
+ListAddLast(List list, T item); -> void
+LIstAdd(List list, T item, int index); -> void
+ListRemoveFirst(List list); -> void
+ListRemoveLast(List list); -> void
+ListRemove(List list, int index); -> void
+ListGetFirst() -> T
+ListGetLast() -> T
+ListGet(int index) -> T*/
+
+#define concat(a,b) a ## b
+
+#define defListType(T) /
+    //T should be a struct /
