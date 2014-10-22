25526688a2b5f2abe9445f30f2899eef27137093
aucguy <jhucguy@gmail.com>
2014-10-22 17:54:52 -0500

    reimplemented base.c since robotc has no pointers
    *facepalm*

--- base.c
+++ base.c
@@ -1,16 +1,49 @@
-#include "list.h" //TODO create this header
+/*refer to doc/base guide.txt on instructions on how to use this
+TODO add header file*/
 
-typedef (bool)(funcUpdate*)(float)
+#define baseName(action, name) __RoBoT ## action ## name
+	/*internally used function. Conacts __RoBot to action to name*/
 
-typedef struct
+#define declareAction(action) \
+	/*call this function with the arguement of the action function to declare an action*/ \
+	bool basename(action, Activated);
+
+#define isActionExecuting(action) \
+	/*'returns' whether or the given action is executing*/\
+	basename(action, Activated);
+
+
+#define updateAction(action) \
+	/*calls the action if it is currently executing. Should be called once per tick*/
+	if(isActionExecuting(action)) \
+	{ \
+		if(callAction(action)) \
+		{ \
+			stopAction(action); \
+		} \
+	} \
+
+#define callAction(action) \
+	/*internally used function. Calls the action no matter what*/
+	action();
+
+#define startAction(action) \
+	/*starts executing the function*/
+	isActionExecuting(action) = true;
+
+#define stopAction(action) \
+	/*stops executing the function*/
+	isActionExecuting(action) = false;
+
+void initRobot()
 {
-    //basic action
-    funcUpdate update; //called each tick args: time returns: whether to continue function or not
-} Action;
-
-defListType(Action);
-ActionList* actions;
-
-void initRobot(); //initializes base.c
-void updateRobot(); //calls each action->update
-void addAction(updateFunc); //adds an action
+	/*this should be called just in case this is actually neccessary.
+	Obviously this currently does nothing.*/
+}
+void updateRobot()
+{
+	/*call this function each tick, since this updates each action
+	For each action you add, add the following line of code here:
+		updateAction(action)
+	where action is the name of the action function*/
+}
